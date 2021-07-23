cd8a167510dee2416b4b59e1e384f2a70c241538
arkaik2k01 <80938348+arkaik2k01@users.noreply.github.com>
2021-07-23 15:45:15 -0400

    Built & Ran GUI. Work on pseudocode & figure out how tables work

--- src/main/java/ucf/assignment/App.java
+++ src/main/java/ucf/assignment/App.java
@@ -10,11 +10,15 @@ public class App extends Application
 {
     @Override
     public void start(Stage primaryStage) throws Exception{
-
+        Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Inventory");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args)
     {
-
+        launch(args);
     }
 }
