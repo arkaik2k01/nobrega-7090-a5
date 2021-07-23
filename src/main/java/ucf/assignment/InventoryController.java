739d6f36bc33e7e67da27300484c8fb435cfcffd
arkaik2k01 <80938348+arkaik2k01@users.noreply.github.com>
2021-07-23 16:04:19 -0400

    Pseudo code for controller done

--- src/main/java/ucf/assignment/InventoryController.java
+++ src/main/java/ucf/assignment/InventoryController.java
@@ -34,32 +34,56 @@ public class InventoryController {
 
     @FXML
     void deleteItem(ActionEvent event) {
-
+        //Get selected item from table
+        //Remove item from observable list
     }
 
     @FXML
     void editName(ActionEvent event) {
-
+        //Save previous name to String
+        //Get new string from table
+        //If greater than 256 or less than 2 chars
+            //return
+        //Set item name to new string
     }
 
     @FXML
     void editPrice(ActionEvent event) {
-
+        //Save previous price to BigDecimal vars
+        //Get new price from table
+        //If not a number
+            //return
+        //Set item price to new price
     }
 
     @FXML
     void editSerial(ActionEvent event) {
-
+        //SAve previous serial to String vars
+        //Get new string from table
+        //If new string not 8 chars in length
+            //return
+        //iterate through tableView
+            //if serial number to change == existing serial number
+                //return
+        //Set item serial to new serial
     }
 
     @FXML
     void searchByName(ActionEvent event) {
-
+        //Get name to search from text field
+        //iterate through tableview
+            //If name equals Item from list
+                //update table view and scroll to item
+        //Send alert "No item with name found
     }
 
     @FXML
     void searchBySerial(ActionEvent event) {
-
+        //Get serial to search from text field
+        //iterate through tableview
+            //If serial equals Item from list
+                //update table view and scroll to item
+        //Send alert "No item with serial found"
     }
 
 }
