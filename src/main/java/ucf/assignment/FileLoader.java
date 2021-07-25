df8c393e90e07fa081333060eca929c6d0d39691
arkaik2k01 <80938348+arkaik2k01@users.noreply.github.com>
2021-07-25 15:22:40 -0400

    Created HTML writer & loader

--- src/main/java/ucf/assignment/FileLoader.java
+++ src/main/java/ucf/assignment/FileLoader.java
@@ -3,10 +3,15 @@ package ucf.assignment;
 import com.google.gson.Gson;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -65,6 +70,42 @@ public class FileLoader extends FileUtil
     @Override
     public ObservableList<Item> readFromHTML()
     {
-        return null;
+        ObservableList<Item> list;
+        //Read the HTML into a jsoup document
+        Document doc = Jsoup.parse(readHTMLintoString());
+        //From ArrayList created by Jsoup based on HTML doc, make a new observableArrayList
+        list = FXCollections.observableArrayList(createArrayListFromHTMLDoc(doc));
+
+        return list;
+    }
+
+    private ArrayList<Item> createArrayListFromHTMLDoc(Document doc)
+    {
+        ArrayList<Item> tempList = new ArrayList<>();
+        for(Element table : doc.select("table"))
+        {
+            //Skipping the header, iterate through each row
+            for(Element row : table.select("tr").subList(1, table.select("tr").size()))
+            {
+                Elements infos = row.select("td");
+                //Get information from row and create an item with info from row
+                Item newItem = new Item(infos.get(0).text(), infos.get(1).text(), new BigDecimal(infos.get(2).text()));
+                //Add new item to list
+                tempList.add(newItem);
+            }
+        }
+        //Return arrayList
+        return tempList;
+    }
+
+    public String readHTMLintoString()
+    {
+        StringBuilder toReturn = new StringBuilder();
+
+        while (in.hasNextLine())
+        {
+            toReturn.append(in.nextLine());
+        }
+        return toReturn.toString();
     }
 }
--- src/main/java/ucf/assignment/FileSaver.java
+++ src/main/java/ucf/assignment/FileSaver.java
@@ -3,6 +3,9 @@ package ucf.assignment;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import javafx.collections.ObservableList;
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -27,7 +30,43 @@ public class FileSaver extends FileUtil
     @Override
     public boolean readToHTML()
     {
+        //Create a html file document
+        Document saveFile = buildDocument();
+        //Write to file using document template
+        try {
+            saveFile.write(out);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
         return true;
+    }
+
+    private Document buildDocument()
+    {
+        //Build html file
+        Document saveFile = DocumentHelper.createDocument();
+        Element html = saveFile.addElement("html");
+        Element head = html.addElement("head");
+        head.addElement("title").addText("Inventory");
+
+        Element body = html.addElement("body");
+        //Create table to print to HTML
+        Element table = body.addElement("table");
+        Element tableHeader = table.addElement("tr");
+        tableHeader.addElement("th").addText("Name");
+        tableHeader.addElement("th").addText("Serial Number");
+        tableHeader.addElement("th").addText("Price");
+        //Add each item from list to table
+        for(Item item : list)
+        {
+            Element tableRow = table.addElement("th");
+            tableRow.addElement("th").addText(item.getName());
+            tableRow.addElement("th").addText(item.getSerialNumber());
+            tableRow.addElement("th").addText(item.getPrice());
+        }
+        return saveFile;
     }
 
     @Override
@@ -60,7 +99,7 @@ public class FileSaver extends FileUtil
         try {
             list.forEach(item -> {
                 try {
-                    out.write(item.name + "\t" + item.getSerialNumber() + "\t" + item.getPrice());
+                    out.write(item.name + "\t" + item.getSerialNumber() + "\t" + item.getPrice()+"\n");
                 } catch (IOException e) {
                     e.printStackTrace();
                     isSuccessful[0] = false;
