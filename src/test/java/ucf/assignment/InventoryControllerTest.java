78a601ef06e512417a57d2b557a641c5cfab88ad
arkaik2k01 <80938348+arkaik2k01@users.noreply.github.com>
2021-07-25 19:02:42 -0400

    JSON loading done. Testing methods next.

---
+++ src/test/java/ucf/assignment/InventoryControllerTest.java
@@ -0,0 +1,30 @@
+package ucf.assignment;
+
+import javafx.scene.control.TableView;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryControllerTest
+{
+
+    @Test
+    void checkIfSerialNotValid()
+    {
+    }
+
+    @Test
+    void checkIfNameNotValid()
+    {
+    }
+
+    @Test
+    void findName()
+    {
+    }
+
+    @Test
+    void findSerial()
+    {
+    }
+}
